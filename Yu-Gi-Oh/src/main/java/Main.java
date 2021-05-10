d078ac5dc56d474d36d2f4050ad75ac526ee84b8
matin <matadysa@gmail.com>
2021-05-11 00:05:30 +0430

    Json files added

--- Yu-Gi-Oh/src/main/java/Main.java
+++ Yu-Gi-Oh/src/main/java/Main.java
@@ -4,8 +4,6 @@ import models.cards.MonsterCard;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(MonsterCard.addMonsterCardFromJSON());
-        System.out.println(MagicCard.addMagicCardFromJSON());
         Core core = new Core();
         core.run();
     }
